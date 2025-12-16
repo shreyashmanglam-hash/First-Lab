3116f03fbe90994c9c86c9c278189525da34948a
shreyashmanglam-hash <shreyashmanglam@gmail.com>
2025-12-16 21:02:33 +0530

    Add Info

--- src/Info.java
+++ src/Info.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Info {
+    public static void main(String[] args) {
+        System.out.println("Name: Shreyash Manglam");
+        System.out.println("Roll number: 24051053");
+        System.out.println("Branch: CSE");
+        System.out.println("Section: 07");
+    }
 }
